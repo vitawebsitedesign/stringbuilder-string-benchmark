ee9735cae955056d1dbeddcce33c518d04faa7b4
Michael Nguyen <mn@3dsafety.com.au>
2019-08-05 18:36:42 +1000

    initial commit

---
+++ StringVsStringbuilder/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+
+namespace StringVsStringbuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Benchmark(DoStringBuilder, 100000);
+            Benchmark(DoString, 100000);
+            Console.WriteLine("---");
+            Benchmark(DoStringBuilder, 110000);
+            Benchmark(DoString, 110000);
+            Console.WriteLine("---");
+            Benchmark(DoStringBuilder, 120000);
+            Benchmark(DoString, 120000);
+            Console.WriteLine("---");
+            Benchmark(DoStringBuilder, 130000);
+            Benchmark(DoString, 130000);
+            Console.WriteLine("---");
+            Benchmark(DoStringBuilder, 140000);
+            Benchmark(DoString, 140000);
+            Console.WriteLine("---");
+            Benchmark(DoStringBuilder, 150000);
+            Benchmark(DoString, 150000);
+            Console.WriteLine("---");
+            Benchmark(DoStringBuilder, 200000);
+            Benchmark(DoString, 200000);
+            Console.WriteLine("---");
+            Benchmark(DoStringBuilder, 300000);
+            Benchmark(DoString, 300000);
+            Console.ReadKey();
+        }
+
+        private static void Benchmark(Action<int> action, int loop)
+        {
+            var sw = Stopwatch.StartNew();
+            action(loop);
+            sw.Stop();
+            Console.WriteLine($"{action.Method.Name} ran {loop} times and took {sw.ElapsedTicks} ticks");
+        }
+
+        private static void DoString(int loop)
+        {
+            string str = "a";
+            for (var i = 0; i < loop; i++)
+            {
+                str += "a";
+            }
+        }
+
+        private static void DoStringBuilder(int loop)
+        {
+            var sb = new StringBuilder("a");
+            for (var i = 0; i < loop; i++)
+            {
+                sb.Append("a");
+            }
+        }
+    }
+}
